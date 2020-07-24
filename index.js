306abf7371b8edc4ea32f7ef3140c8c50b04e0a2
obasemola <obasemolaayodele@gmail.com>
2020-07-24 12:19:55 +0300

    identifying clicked operator keys

--- index.js
+++ index.js
@@ -1,14 +1,26 @@
 const keys = document.querySelector('#calculatorKeys');
 const display = document.querySelector('#calculatorDisplay');
 
-let key;
-let action;
+
 keys.addEventListener('click', (e) => {
   if(e.target.matches('button')) {
     key = e.target;
     keyAction = key.dataset.action;
     keyNum = key.textContent;
     displayedNum = display.textContent;
+    if (
+      keyAction === 'add' ||
+      keyAction === 'subtract' ||
+      keyAction === 'divide' ||
+      keyAction === 'multiply'
+      ) {
+        key.classList.add('pressed');
+        calculator.dataset.typeOfPreviousKey = 'operator';
+      }
+    const keyArray = Array.from(key.parentNode.children);
+    keyArray.forEach((key) => {
+      key.classList.remove('pressed');
+    });
     // console.log(keyAction, keyNum, displayedNum)
   }
 
@@ -34,4 +46,7 @@ keys.addEventListener('click', (e) => {
     ) {
       key.classList.add('pressed');
     }
+
+
 });
+
